d880c9d66411c24500d92ea76b9b539a98a90c55
Che-Ming Chang <43882897+m3gofriends@users.noreply.github.com>
2022-12-01 17:43:14 +0800

    Update yield_calculator.py

--- yield_calculator.py
+++ yield_calculator.py
@@ -15,15 +15,15 @@ def now_yield(stock_code, language='en'):
     cash_dividend = float(dom.xpath('//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[2]/div/div/div[2]/ul/li[1]/div/div[2]/span')[0].text)
     stock_dividend = float(dom.xpath('//*[@id="main-2-QuoteDividend-Proxy"]/div/section[2]/div[2]/div/div/div[2]/ul/li[1]/div/div[3]/span')[0].text)
     if language == 'zh-TW':
-        print(stock_code + "  當前股價:" + str(share_price) + ",  現金殖利率:" + str(round(cash_dividend *100 /share_price, 1)) + "%,  股票殖利率:" + str(round(stock_dividend *100 /share_price, 1)) + "%\n")
+        print(stock_code + "  當前股價:" + str(share_price) + ",  現金殖利率:" + str(round(cash_dividend * 100 / share_price, 1)) + "%,  股票殖利率:" + str(round(stock_dividend * 100 / share_price, 1)) + "%\n")
     elif language == 'zh-CN':
-        print(stock_code + "  当前股价:" + str(share_price) + ",  现金殖利率:" + str(round(cash_dividend *100 /share_price, 1)) + "%,  股票殖利率:" + str(round(stock_dividend *100 /share_price, 1)) + "%\n")
+        print(stock_code + "  当前股价:" + str(share_price) + ",  现金殖利率:" + str(round(cash_dividend * 100 / share_price, 1)) + "%,  股票殖利率:" + str(round(stock_dividend * 100 / share_price, 1)) + "%\n")
     elif language == 'jp':
-        print(stock_code + "  現在の株価:" + str(share_price) + ",  配当利回り:" + str(round(cash_dividend *100 /share_price, 1)) + "%,  株式利回り:" + str(round(stock_dividend *100 /share_price, 1)) + "%\n")
+        print(stock_code + "  現在の株価:" + str(share_price) + ",  配当利回り:" + str(round(cash_dividend * 100 / share_price, 1)) + "%,  株式利回り:" + str(round(stock_dividend * 100 / share_price, 1)) + "%\n")
     elif language == 'kr':
-        print(stock_code + "  현재 주가:" + str(share_price) + ",  현금수익률:" + str(round(cash_dividend *100 /share_price, 1)) + "%,  주식 수익률:" + str(round(stock_dividend *100 /share_price, 1)) + "%\n")
+        print(stock_code + "  현재 주가:" + str(share_price) + ",  현금수익률:" + str(round(cash_dividend * 100 / share_price, 1)) + "%,  주식 수익률:" + str(round(stock_dividend * 100 / share_price, 1)) + "%\n")
     else:
-        print(stock_code + "  Current share price:" + str(share_price) + ",  Cash yield:" + str(round(cash_dividend *100 /share_price, 1)) + "%,  Stock yield:" + str(round(stock_dividend *100 /share_price, 1)) + "%\n")
+        print(stock_code + "  Current share price:" + str(share_price) + ",  Cash yield:" + str(round(cash_dividend * 100 / share_price, 1)) + "%,  Stock yield:" + str(round(stock_dividend * 100 / share_price, 1)) + "%\n")
         
 now_yield('2330.TW', 'zh-TW')
 now_yield('2882.TW', 'zh-TW')
